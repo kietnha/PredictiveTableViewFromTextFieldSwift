7744c8ca76592e7b73654f64d509aa4694d08083
Matthew Howes Singleton <Singleton@matthews-macbook-pro.local>
2016-05-26 01:07:41 -0700

    Initial Commit

---
+++ PredictiveTextTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PredictiveTextTableView
+//
+//  Created by Matthew Howes Singleton on 5/26/16.
+//  Copyright © 2016 Matthew Howes Singleton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
